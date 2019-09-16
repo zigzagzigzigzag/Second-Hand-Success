44ec48d82427e868cd5a0c456fbe712b294176c0
zigzagzigzigzag <51709377+zigzagzigzigzag@users.noreply.github.com>
2019-09-16 12:00:33 +0200

    Added an admin home page.

---
+++ SecondHandSuccess2/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using SecondHandSuccess2.Models;
+using System.Data.Entity;
+
+namespace SecondHandSuccess2.Controllers
+{
+    public class AdminController : Controller
+    {
+       
+        Model model = new Model();
+
+        public ActionResult AdminHome()
+        {
+
+            ViewBag.Modules = model.Modules;
+            ViewBag.People = model.People;
+            return View();
+        }
+
+
+    }
+}
