3f1a93f452d77d218360a2e5ee637fad4fcac2d6
claudiamirna98 <claudiamirna89@gmail.com>
2019-10-23 16:46:54 +0200

    got lots done
    yasssssss

--- SecondHandSuccess2/Controllers/HomeController.cs
+++ SecondHandSuccess2/Controllers/HomeController.cs
@@ -10,17 +10,15 @@ namespace SecondHandSuccess2.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
-            Session.Contents.RemoveAll();
-            return RedirectToAction("LogIn","Home");
+            //Session.Contents.RemoveAll();
+            return RedirectToAction("LogIn", "Home");
         }
 
         public ActionResult LogIn()
         {
-            if(ViewBag.passError == null)
-            ViewBag.passError = "";
-
             return View();
         }
 
@@ -54,25 +52,54 @@ namespace SecondHandSuccess2.Controllers
             }
         }
 
-       public ActionResult EditListing()
-       {      
-            return View();
-       }
+        public ActionResult EditListing()
+        {
+            if (Session["User"] != null)
+            {
+                string bookISBN = RouteData.Values["id"].ToString();
+                string uId = ((PERSON)Session["User"]).PersonIDNumber;
+
+                Listing curList = model.Listings.Find(bookISBN, uId);
+                Session["currentListing"] = curList;
+
+
+                List<String> conditions = new List<string> { "Very Bad", "Bad", "Average", "Good", "Very Good" };
+
+                ViewBag.books = model.BOOKs;
+
+
+                ViewData["conditions"] =
+                    new SelectList(new[] { "Very Bad", "Bad", "Average", "Good", "Very Good" }
+                    .Select(x => new { value = x, text = x }),
+                    "value", "text", "Very Bad");
+
+                return View(curList);
+            }
+
+            else
+            {
+                return RedirectToAction("LogIn", "Home");
+            }
+        }
 
         public ActionResult AddListing()
         {
+            if (Session["User"] != null)
+            {
+                List<String> conditions = new List<string> { "Very Bad", "Bad", "Average", "Good", "Very Good" };
 
-            List<String> conditions = new List<string> { "Very Bad", "Bad", "Average", "Good", "Very Good" };
+                ViewData["conditions"] =
+                    new SelectList(new[] { "Very Bad", "Bad", "Average", "Good", "Very Good" }
+                    .Select(x => new { value = x, text = x }),
+                    "value", "text", "Very Bad");
 
-            ViewBag.books = model.BOOKs;
+                return View();
+            }
 
-
-            ViewData["conditions"] =
-                new SelectList(new[] { "Very Bad", "Bad", "Average", "Good", "Very Good" }
-                .Select(x => new { value = x, text = x }),
-                "value", "text", "Very Bad");
-
-            return View();
+            else
+            {
+                return RedirectToAction("LogIn", "Home");
+            }
         }
 
         [HttpPost]
@@ -124,7 +151,7 @@ namespace SecondHandSuccess2.Controllers
             String publisher = Request.Form["book.bookPublisher"];
             String author = Request.Form["book.bookAuthor"];
             String edition = Request.Form["book.bookEdition"];
-            
+
             bool exists = false;
             foreach (BOOK book in model.BOOKs)
             {
@@ -148,29 +175,58 @@ namespace SecondHandSuccess2.Controllers
                     model.BOOKs.Add(book);
                     model.SaveChanges();
                 }
-                
+
             }
-
             return RedirectToAction("AddListing", "Home");
-
-
         }
-    
-
-    public ActionResult getView()
-    {
-        return View(model.BOOKs);
-    }
 
 
-    [HttpPost]
+        public ActionResult getView()
+        {
+            return View(model.BOOKs);
+        }
+        public ActionResult deleteListing() {
+
+            string bookISBN = RouteData.Values["id"].ToString();
+            string uId = ((PERSON)Session["User"]).PersonIDNumber;
+            Listing toRemove = model.Listings.Find(bookISBN, uId);
+            model.Listings.Remove(toRemove);
+            model.SaveChanges();
+
+            return RedirectToAction("UserHomePage", "Home");
+        }
+        public ActionResult editListingPage() {
+            string condition = Request.Form["conditions"];
+            var price = Request.Form["listingPrice"];
+            var ISBN = Request.Form["bookISBN"];
+            String date = Request.Form["listingDate"];
+            string idNumber = Request.Form["personIDNumber"];
+
+            DateTime dateToUse = DateTime.UtcNow;
+
+            Listing current = (Listing)Session["currentListing"];
+            model.Listings.Find(current.bookISBN, current.personIDNumber).listingCondition = condition;
+            model.Listings.Find(current.bookISBN, current.personIDNumber).listingPrice = price;
+            model.Listings.Find(current.bookISBN, current.personIDNumber).bookISBN = ISBN;
+            model.Listings.Find(current.bookISBN, current.personIDNumber).listingDate = dateToUse;
+            model.Listings.Find(current.bookISBN, current.personIDNumber).personIDNumber = idNumber;
+            model.SaveChanges();
+
+            return RedirectToAction("UserHomePage", "Home");
+        }
+
         public ActionResult LogOn()
         {
+            Boolean uNameFound = false;
+            Session["uPasswordError"] = "";
+            Session["uNameError"] = "";
             String uName = Request.Form["PersonUserName"];
             String uPassword = Request.Form["PersonPassword"];
             foreach (PERSON curP in model.People) {
                 if (curP.PersonUserName == uName)
-            { if (curP.PersonPassword == uPassword)
+                {
+                   // uNameFound = true; 
+                    if (curP.PersonPassword == uPassword)
                     {
                         Session["User"] = curP;
                         personID = curP.PersonIDNumber;
@@ -186,14 +242,16 @@ namespace SecondHandSuccess2.Controllers
                         {
                             return RedirectToAction("AdminHome", "Admin");
                         }
-
                     }
-                    else {
-                        ViewBag.passError = "Incorrect";
+                    else
+                    {
+                        ModelState.AddModelError("PersonUserName", "Error message for User");
+                        Session["uPasswordError"] = "Wrong Password";
                         return RedirectToAction("LogIn", "Home");
                     }
                 }
             }
+            if (!uNameFound) { Session["uNameError"] = "Wrong Username"; }
             return RedirectToAction("LogIn", "Home");
         }
 
